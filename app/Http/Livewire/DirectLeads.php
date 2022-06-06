99d7499ce02d343209ed3fafa0b41499264c9234
rtrenuela <rtrenuela@objectbright.com>
2022-06-06 14:20:08 +0800

    update

--- app/Http/Livewire/DirectLeads.php
+++ app/Http/Livewire/DirectLeads.php
@@ -24,5 +24,7 @@ class DirectLeads extends Component
                 ]);
             }
         }
+
+        $this->emit('refreshDatatable');
     }
 }
--- resources/views/livewire/direct-leads.blade.php
+++ resources/views/livewire/direct-leads.blade.php
@@ -5,7 +5,12 @@
                 <a href="#" class="btn btn-success" wire:click="reSync">Re-sync my Leads</a>
             </div>
             <div class="col-auto overflow-auto">
-                <livewire:direct-leads-table/>
+                <div wire:loading.remove>
+                    <livewire:direct-leads-table/>
+                </div>
+                <div wire:loading>
+                    <i class="fas fa-spinner"></i>
+                </div>
             </div>
         </div>
     </div>
