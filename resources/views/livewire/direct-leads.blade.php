eca2cfd4e04ec92c5fa482758a2392f748391fcc
rtrenuela <rtrenuela@objectbright.com>
2022-06-06 14:25:06 +0800

    update

--- resources/views/livewire/direct-leads.blade.php
+++ resources/views/livewire/direct-leads.blade.php
@@ -2,15 +2,17 @@
     <div class="mt-5">
         <div class="row">
             <div class="col-12 mb-2">
-                <a href="#" class="btn btn-success" wire:click="reSync">Re-sync my Leads</a>
-            </div>
-            <div class="col-auto overflow-auto">
                 <div wire:loading.remove>
-                    <livewire:direct-leads-table/>
+                    <a href="#" class="btn btn-success" wire:click="reSync">Re-sync my Leads</a>
                 </div>
                 <div wire:loading>
-                    <i class="fas fa-spinner"></i>
+                    <div class="spinner-border" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
                 </div>
+            </div>
+            <div class="col-auto overflow-auto">
+                <livewire:direct-leads-table/>
             </div>
         </div>
     </div>
